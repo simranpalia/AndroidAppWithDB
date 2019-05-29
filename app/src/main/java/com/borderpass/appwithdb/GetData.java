e91769b6db3eb14bf8e957caf76c3f4c47777480
Simran <simran_palia@yahoo.co.in>
2019-05-29 16:13:45 +0800

    Get data activity view completed.

--- app/src/main/java/com/borderpass/appwithdb/GetData.java
+++ app/src/main/java/com/borderpass/appwithdb/GetData.java
@@ -1,7 +1,9 @@
 package com.borderpass.appwithdb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class GetData extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class GetData extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_data);
+    }
+
+    public void startMainActivity(View view) {
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
