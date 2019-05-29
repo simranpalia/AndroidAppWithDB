db3596e1563acb070b43ba242cfe974f14ffec58
Simran <simran_palia@yahoo.co.in>
2019-05-29 16:18:40 +0800

    Add new data activity added.

---
+++ app/src/main/java/com/borderpass/appwithdb/AddData.java
@@ -0,0 +1,20 @@
+package com.borderpass.appwithdb;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class AddData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_data);
+    }
+
+    public void startMainActivity(View view) {
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+}
