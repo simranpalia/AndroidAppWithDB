9e3b8f3a9eb4b42d826da7cc7c21db5c419defc9
Simran <simran_palia@yahoo.co.in>
2019-05-29 16:02:07 +0800

    Get data activity added.

--- app/src/main/java/com/borderpass/appwithdb/GetData.java
+++ app/src/main/java/com/borderpass/appwithdb/GetData.java
@@ -2,18 +2,12 @@ package com.borderpass.appwithdb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class GetData extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-    }
-
-    public void startGetData(View view) {
-
+        setContentView(R.layout.activity_get_data);
     }
 }
