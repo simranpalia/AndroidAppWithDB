3496c663524126b23ab63c735390f6b1f905b697
Simran <simran_palia@yahoo.co.in>
2019-05-29 16:05:27 +0800

    Intent created to start new activity.

--- app/src/main/java/com/borderpass/appwithdb/MainActivity.java
+++ app/src/main/java/com/borderpass/appwithdb/MainActivity.java
@@ -1,5 +1,6 @@
 package com.borderpass.appwithdb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,10 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
     public void startGetData(View view) {
+        Intent intent=new Intent(this,GetData.class);
+        startActivity(intent);
 
     }
 }
